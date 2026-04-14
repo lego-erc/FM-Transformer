1abc8357317f6d9def09e302368aa0afcd4be223
rdebrand <richard.hildebrandt@tum.de>
2026-04-14 14:09:18 +0200

    fix cuda

--- scripts/train_setup.sh
+++ scripts/train_setup.sh
@@ -1,6 +1,5 @@
 MARKER="$CONDA_PREFIX/.train-setup-done"
 if [ ! -f "$MARKER" ]; then
-    uv pip install torch --index-url https://download.pytorch.org/whl/cu128 --reinstall \
-    && uv pip install -q 'git+https://github.com/rdebrand/torch-lap-cuda' \
+    uv pip install -q 'git+https://github.com/rdebrand/torch-lap-cuda' \
     && touch "$MARKER"
 fi
