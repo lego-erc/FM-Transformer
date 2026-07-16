66ac1205cfbb7d6938985b212e0a9d5dda84500c
rdebrand <richard.hildebrandt@tum.de>
2026-07-16 16:51:11 +0200

    fix train

--- scripts/train.py
+++ scripts/train.py
@@ -126,7 +126,7 @@ if hasattr(model, "base_head"):
     model.rc.config["base_conf"]["base_head"] = {
         k: v.cpu() for k, v in model.base_head.state_dict().items()
     }
-    model.rc.config["base_conf"]["base_head_frozen"] = not model.base_head.weight.requires_grad
+    model.rc.config["base_conf"]["base_head_frozen"] = not model.base_head[-1].weight.requires_grad
 
 ckpt_base = run.get("ckpt_dir") or os.environ.get("LEGO_CKPT_DIR", "./checkpoints/")
 ckpt_dir = os.path.join(ckpt_base, "flow" if train_model == "fm" else "mult")
