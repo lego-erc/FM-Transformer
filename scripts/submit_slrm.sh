67b75d691e94b519cda1ae41d3e522078cddcffe
Richard Hildebrandt <richard.hildebrandt@tum.de>
2026-08-17 22:42:55 +0200

    update slurm file

--- scripts/submit_slrm.sh
+++ scripts/submit_slrm.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -l
+#SBATCH -o /ptmp/hildebra/lego_fm_models/job.out.%j
+#SBATCH -e /ptmp/hildebra/lego_fm_models/job.err.%j
+#SBATCH -D /u/hildebra/phd/legofmt/FM-Transformer
 #SBATCH -J train_legofmt
+#SBATCH --constraint="gpu"
+#SBATCH --nvmps
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --ntasks-per-node=4
-#SBATCH --mem=125000
-#SBATCH --time=0-02:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mail-type=none
+#SBATCH --mail-user=richard.hildebrandt@tum.de
+#SBATCH --time=4:59:00
 
-srun pixi run train
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+CONFIG="${1:-fm_default}"
+
+srun pixi run --frozen -e train-comet python scripts/train.py "$CONFIG"
