c34a74fa1767cd3ab570ea47f89152620980ae19
Richard Hildebrandt <richard.hildebrandt@tum.de>
2026-09-06 18:46:53 +0200

    caching for rotation

--- legofmt/geometry/symmetry_projections.py
+++ legofmt/geometry/symmetry_projections.py
@@ -17,13 +17,23 @@ class CubeSymmetry:
         [[0, 0, -1], [1, 0, 0], [0, -1, 0]],
     ], dtype=torch.float32)
 
+    def __init__(self) -> None:
+        self._cache: dict = {}
+
+    def _rot(self, like: torch.Tensor) -> torch.Tensor:
+        key = (like.device, like.dtype)
+        r = self._cache.get(key)
+        if r is None:
+            r = self._cache[key] = self._R.to(like)
+        return r
+
     def face_of(self, pos):
         axis = pos.abs().argmax(-1)
         neg = pos.gather(-1, axis.unsqueeze(-1)).squeeze(-1) < 0
         return 2 * axis + neg.long()
 
     def canonicalize(self, dirs, face):
-        return torch.einsum("b...i,bij->b...j", dirs, self._R.to(dirs)[face].mT)
+        return torch.einsum("b...i,bij->b...j", dirs, self._rot(dirs)[face].mT)
 
     def uncanonicalize(self, dirs, face):
-        return torch.einsum("b...i,bij->b...j", dirs, self._R.to(dirs)[face])
+        return torch.einsum("b...i,bij->b...j", dirs, self._rot(dirs)[face])
